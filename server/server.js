a051bf04e3158524af9a564693c084230e662850
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2020-11-27 16:04:39 +1100

    Increase Request Payload Limit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const apiRouter = require('./routes')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '5mb'}))
 app.use(express.static('build'))
 app.use(apiRouter)
 
