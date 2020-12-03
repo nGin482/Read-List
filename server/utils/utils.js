36d1a2dae726cede023ee41b383a8454bc80af3b
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2020-12-03 17:40:54 +1100

    Cleaned up uncessary code

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -4,7 +4,6 @@ const validateAO3Record = record => {
     record.chapters = record.chapters.substring(0, record.chapters.indexOf('/'))
     record.words = Number(record.words.replace(',', ''))
     record.storyID = record.url.substring((record.url.lastIndexOf('/')+1))
-    console.log(record.date.split('/'))
     updatedDate = record.date.split('/')
     record.updatedDate = new Date(new Date().getFullYear(), Number(updatedDate[1])-1, Number(updatedDate[0])+1)
 
@@ -13,12 +12,9 @@ const validateAO3Record = record => {
 
 const validateFFNRecord = record => {
     record.words = Number(record.words.replace(',', ''))
-    record.relationships = record.characters.pairings
-    record.characters = record.characters.characters
     record.title = record.title
     updatedDate = record.updatedDate.split('/')
     record.updatedDate = new Date(new Date().getFullYear(), Number(updatedDate[1])-1, Number(updatedDate[0])+1)
-    console.log(record.publishedDate)
     publishedDate = record.publishedDate.split('/')
     record.publishedDate = new Date(new Date().getFullYear(), Number(publishedDate[1])-1, Number(publishedDate[0])+1)
 
@@ -68,9 +64,6 @@ const searchAllStoriesByKey = (key, expected) => {
             })
         })
     }
-    // const result2 = stories.map(day => {
-    //     day.stories.filter(story => story[key].includes(expected))
-    // }) // returns undefined
 
     return result
 }
