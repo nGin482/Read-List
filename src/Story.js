87686544b9d0b3629bc129e31d6aa350f8462340
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-01-21 21:53:28 +1100

    Styles for the Story-Card

--- src/Story.js
+++ src/Story.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Story.css';
 
 const Story = (story) => {
     console.log(story)
@@ -13,24 +14,28 @@ const Story = (story) => {
             )
         }
         return (
-            <div>
-                <h3>{thisStory.title}</h3>
-                <p>{thisStory.author}</p>
-                <p>{thisStory.summary}</p>
-                <ul>
-                    {thisStory.characters.map(character => <li key={character}>{character}</li>)}
-                </ul>
-                <p>{thisStory.relationships}</p>
-                <p>{thisStory.fandoms}</p>
-                <p>{thisStory.chapters}</p>
-                <p>{thisStory.words}</p>
-                <p>{thisStory.publishedDate}</p>
-                <p>{thisStory.updatedDate}</p>
-                <p>{thisStory.genres}</p>
-                <p>{thisStory.rating}</p>
-                <p>{thisStory.status}</p>
-                <p>{thisStory.archive}</p>
-                <a href={thisStory.url}>Link to this story</a>
+            <div className="story-card">
+                <h3 id="story-title">{thisStory.title}</h3>
+                <dl>
+                    <dt>Author:</dt><dd>{thisStory.author}</dd>
+                    <dt>Summary:</dt><dd>{thisStory.summary}</dd>
+                    <dt>Characters:</dt>
+                        {thisStory.characters.length > 0 ? <dd>{thisStory.characters.map(character => <li key={character}>{character}</li>)}</dd> : <dd>No characters were tagged</dd>}
+                    <dt>Relationships:</dt>
+                        {thisStory.relationships.length > 0 ? <dd>{thisStory.relationships.map(rel => <li key={rel}>{rel}</li>)}</dd> : <dd>No relationships were tagged</dd>}
+                    <dt>Fandoms:</dt>
+                        {thisStory.fandoms.length > 0 ? <dd>{thisStory.fandoms}</dd> : <dd>No fandoms were tagged</dd>}
+                    <dt>Chapters:</dt><dd>{thisStory.chapters}</dd>
+                    <dt>Words:</dt><dd>{thisStory.words}</dd>
+                    <dt>Published Date:</dt><dd>{thisStory.publishedDate}</dd>
+                    <dt>Updated Date:</dt><dd>{thisStory.updatedDate}</dd>
+                    <dt>Genres:</dt>
+                    {thisStory.genres.length > 0 ? <dd>{thisStory.genres.map(genre => <li key={genre}>{genre}</li>)}</dd> : <dd>No fandoms were tagged</dd>}
+                    <dt>Rating:</dt><dd>{thisStory.rating}</dd>
+                    <dt>Status:</dt><dd>{thisStory.status}</dd>
+                    <dt>Archive:</dt><dd>{thisStory.archive}</dd>
+                    <a href={thisStory.url}>Link to this story</a>
+                </dl>
             </div>
         )
     }
