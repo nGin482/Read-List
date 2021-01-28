79a8b1714336d9a86fe6604653cd9b403008d6e3
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-01-28 15:36:06 +1100

    Controls for filtering display of story list

--- src/StoryList.js
+++ src/StoryList.js
@@ -2,9 +2,17 @@ import React, {useState} from 'react';
 import Story from './Story';
 
 const StoryList = (stories) => {
+    const [archiveFilter, setArchiveFilter] = useState('')
+    
+    const filterStoriesByArchive = () => {
+        return stories.stories.filter(story => story.archive === archiveFilter)   
+    }
+
     return (
         <div id="story-list">
-            {stories.stories.map(story => <Story story={story}/>)}
+            <button onClick={()=> setArchiveFilter('Fanfiction.Net')}>Fanfiction.Net</button>
+            <button onClick={()=> setArchiveFilter('Archive of our Own')}>Archive of our Own</button>
+            {filterStoriesByArchive().map(story => <Story story={story}/>)}
         </div>
     )
 }
