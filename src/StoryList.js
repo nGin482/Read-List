c1efd1ad116e279a087ff40486602892a908cde6
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-01-28 23:08:35 +1100

    Controls for filtering story by fandom

--- src/StoryList.js
+++ src/StoryList.js
@@ -2,17 +2,51 @@ import React, {useState} from 'react';
 import Story from './Story';
 
 const StoryList = (stories) => {
-    const [archiveFilter, setArchiveFilter] = useState('')
+    const [archiveFilter, setArchiveFilter] = useState('All')
+    const [fandomFilter, setFandomFilter] = useState('All')
     
-    const filterStoriesByArchive = () => {
-        return stories.stories.filter(story => story.archive === archiveFilter)   
+    const filterStoriesByArchive = (stories) => {
+        if (archiveFilter === 'All') {
+            return stories
+        }
+        return stories.filter(story => story.archive === archiveFilter)   
+    }
+
+    const allFandoms = () => {
+        return ['Code Geass', 'Doctor Who', 'Endeavour', 'Lewis', 'NCIS', 'NCIS: Los Angeles', 'Person of Interest', 'Transformers']
+    }
+    const filterStoriesByFandom = () => {
+        if (fandomFilter === 'All') {
+            return stories.stories
+        }
+        const matchFandomValues = (fandoms) => {
+            var idx = -1
+            fandoms.map(fandom => {
+                idx = fandom.indexOf(fandomFilter)
+                return ''
+            })
+            if (idx === -1) {
+                return false
+            }
+            else {
+                return true
+            }
+        }
+        return stories.stories.filter(story => matchFandomValues(story.fandoms))
     }
 
     return (
         <div id="story-list">
-            <button onClick={()=> setArchiveFilter('Fanfiction.Net')}>Fanfiction.Net</button>
-            <button onClick={()=> setArchiveFilter('Archive of our Own')}>Archive of our Own</button>
-            {filterStoriesByArchive().map(story => <Story story={story}/>)}
+            <div id="filter-archive">
+                <button onClick={()=> setArchiveFilter('Fanfiction.Net')}>Fanfiction.Net</button>
+                <button onClick={()=> setArchiveFilter('Archive of our Own')}>Archive of our Own</button>
+                <button onClick={()=> setArchiveFilter('All')}>All</button>
+            </div>
+            <div id="filter-fandom">
+                {allFandoms().map(fandom => <button onClick={() => setFandomFilter(fandom)}>{fandom}</button>)}
+                <button onClick={() => setFandomFilter('All')}>All</button>
+            </div>
+            {filterStoriesByFandom().map(story => <Story story={story}/>)}
         </div>
     )
 }
