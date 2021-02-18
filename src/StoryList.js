0bf7ecf14484897900e48f59de3743519f9d4a82
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-02-18 13:44:17 +1100

    Minor changes to filter methods + filter options displayed at bottom

--- src/StoryList.js
+++ src/StoryList.js
@@ -8,7 +8,7 @@ import './StoryList.css';
 
 const StoryList = ({stories}) => {
     const [archiveFilter, setArchiveFilter] = useState('All')
-    const [fandomFilter, setFandomFilter] = useState('All')
+    const [fandomFilter, setFandomFilter] = useState('All Stories')
     const givenDate = stories.date
     
     const filterStoriesByArchive = (stories) => {
@@ -27,7 +27,7 @@ const StoryList = ({stories}) => {
     const filterStoriesByFandom = () => {
         const givenCollection = stories.stories
         const givenStories = []
-        if (fandomFilter === 'All') {
+        if (fandomFilter.includes('All')) {
             for (var i = 0; i < givenCollection.length; i++) {
                 const archive = givenCollection[i]
                 for (var j = 0; j < archive.stories.length; j++) {
@@ -67,7 +67,7 @@ const StoryList = ({stories}) => {
                 </div>
                 <div id="filter-fandom">
                     {allFandoms().map(fandom => <button onClick={() => setFandomFilter(fandom)}>{fandom}</button>)}
-                    <button onClick={() => setFandomFilter('All')}>All</button>
+                    <button onClick={() => setFandomFilter('All Stories')}>All</button>
                 </div>
                 <br/>
                 <div id='filter-results'>
@@ -75,6 +75,15 @@ const StoryList = ({stories}) => {
                     <p id="number"><span>Number of stories:</span><br/>{filterStories().length}</p>
                 </div>
                 {filterStories().length > 0 ? filterStories().map(story => <Story story={story}/>) : <div id='no-story-warning'>There are no stories to view from this date</div>}
+                <div id="filter-archive">
+                    <button onClick={()=> setArchiveFilter('Fanfiction.Net')}>Fanfiction.Net</button>
+                    <button onClick={()=> setArchiveFilter('Archive of our Own')}>Archive of our Own</button>
+                    <button onClick={()=> setArchiveFilter('All')}>All</button>
+                </div>
+                <div id="filter-fandom">
+                    {allFandoms().map(fandom => <button onClick={() => setFandomFilter(fandom)}>{fandom}</button>)}
+                    <button onClick={() => setFandomFilter('All')}>All</button>
+                </div>
             </div>
         )
     }
