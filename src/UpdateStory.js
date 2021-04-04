b01387761f5eac39f712723e32671dc8ab6471f9
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-04-04 19:22:10 +1000

    Submit control and minor changes

--- src/UpdateStory.js
+++ src/UpdateStory.js
@@ -4,30 +4,33 @@ import './UpdateStory.css'
 
 const UpdateStory = ({story}) => {
     const [field, setField] = useState('')
-    const [data, setData] = useState('')
+    const [value, setValue] = useState('')
     const [stored, setStored] = useState('')
-    
-    const handleFieldChange = event => {
+
+    const submitUpdate = event => {
         event.preventDefault()
-        setField(event.target.value)
-    }
-    const handleDataChange = event => {
-        event.preventDefault()
-        setData(event.target.value)
+        const update = {
+            field: field,
+            value: value,
+            stored: stored
+        }
+        console.log(update)
     }
 
     return (
         <div id="update-wrapper">
             <h4 id="update-wrapper-title">Update Story</h4>
-            <select id="data-select" onChange={event => handleFieldChange(event)}>
-                <option>Select the data to change</option>
-                {Object.keys(story).map(key => <option key={key} value={key}>{key}</option>)}
-            </select><br/>
-            <input id="data-change" type="text" placeholder="Your change" value={data} onChange={event => handleDataChange(event)}/><br/>
-            Location stored:
-            <input type="radio" id="file" value="File" onClick={event => setStored(event.target.value)}/>File
-            <input type="radio" id="db" value="Database" onClick={event => setStored(event.target.value)}/>Database
-            <input id="update-submit" type="submit" value="Confirm change?"/>
+            <form onSubmit={event => submitUpdate(event)}>
+                <select id="data-select" onChange={event => setField(event.target.value)}>
+                    <option>Select the data to change</option>
+                    {Object.keys(story).map(key => <option key={key} value={key}>{key}</option>)}
+                </select><br/>
+                <input id="data-change" type="text" placeholder="Your change" value={value} onChange={event => setValue(event.target.value)}/><br/>
+                Location stored:
+                <input type="radio" id="file" value="File" onClick={event => setStored(event.target.value)}/>File
+                <input type="radio" id="db" value="Database" onClick={event => setStored(event.target.value)}/>Database
+                <input id="update-submit" type="submit" value="Confirm change?"/>
+            </form>
         </div>
     )
 }
