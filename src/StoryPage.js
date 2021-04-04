7d6205fd485c0b3902cb849f129e0457e9314693
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-04-04 13:21:02 +1000

    Component for single story shows warning if story not found

--- src/StoryPage.js
+++ src/StoryPage.js
@@ -1,23 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import {useParams, Link, Redirect} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import services from './services/services.js';
 import Story from './Story.js';
 
 const StoryPage = () => {
     const [story, setStory] = useState(null)
+    const [warning, setWarning] = useState('')
     const storyID = useParams().storyID
     
     useEffect(() => {
-        services.getStoriesByID(storyID).then(data => setStory(data))
+        services.getStoriesByID(storyID).then(data => setStory(data)).catch(err => {
+            if (err.response.status === 404) {
+                setWarning('The story could not be found')
+            }
+            else {
+                setWarning('An error occurred')
+            }
+        })
     }, [])
+    console.log(story)
 
     if (story) {
         return (
             <Story story={story}/>
         )
     }
+    else if (warning !== '') {
+        return <div id="story-not-found">The story could not be found</div>
+    }
     else {
-        return <div>The story could not be found</div>
+        return 'Waiting for the story to load ...'
     }
 }
 
