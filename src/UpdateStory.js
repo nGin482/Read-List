d9ecf9fe617af1c5ac8d7961dfbf05f2a82a39fb
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-05-08 22:01:29 +1000

    Misc changes to update form and data validation on submit

--- src/UpdateStory.js
+++ src/UpdateStory.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import services from './services/services.js';
+// import services from './services/services.js';
 import './UpdateStory.css'
 
 const UpdateStory = ({story}) => {
@@ -9,6 +9,8 @@ const UpdateStory = ({story}) => {
 
     const submitUpdate = event => {
         event.preventDefault()
+        // if field is n/a or '' or value = '' or stored = ''
+        // show modal warning
         const update = {
             field: field,
             value: value,
@@ -22,13 +24,13 @@ const UpdateStory = ({story}) => {
             <h4 id="update-wrapper-title">Update Story</h4>
             <form onSubmit={event => submitUpdate(event)}>
                 <select id="data-select" onChange={event => setField(event.target.value)}>
-                    <option>Select the data to change</option>
+                    <option value="n/a">Select the data to change</option>
                     {Object.keys(story).map(key => <option key={key} value={key}>{key}</option>)}
                 </select><br/>
                 <input id="data-change" type="text" placeholder="Your change" value={value} onChange={event => setValue(event.target.value)}/><br/>
-                Location stored:
-                <input type="radio" id="file" value="File" onClick={event => setStored(event.target.value)}/>File
-                <input type="radio" id="db" value="Database" onClick={event => setStored(event.target.value)}/>Database
+                Where is this story stored? <br/>
+                <input type="radio" id="file" value="File" onClick={event => setStored(event.target.value)}/>In-Memory <br/>
+                <input type="radio" id="db" value="DB" onClick={event => setStored(event.target.value)}/>Database
                 <input id="update-submit" type="submit" value="Confirm change?"/>
             </form>
         </div>
