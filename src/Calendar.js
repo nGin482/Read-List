0b7503f91565ddf907d3c690c35d687d270500c4
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-05-11 19:06:02 +1000

    Logic for changing months

--- src/Calendar.js
+++ src/Calendar.js
@@ -5,6 +5,7 @@ const Calendar = (dates) => {
     dates = dates.dates
     const [month, setMonth] = useState('')
     const [date, setDate] = useState('')
+    let monthIdx = 0
 
     const getMonth = (date) => {
         const givenMonth = date.slice(date.indexOf('-')+1, date.lastIndexOf('-'))
@@ -18,15 +19,30 @@ const Calendar = (dates) => {
         datesList.push(monthObj)
     })
     if (month === '') {
-        setMonth(datesList[0].monthName)
+        setMonth(datesList[monthIdx].monthName)
+    }
+    const handleMonthChange = (direction) => {
+        if (direction === 'Next') {
+            if (monthIdx <= datesList.length-1) {
+                monthIdx++
+            }
+        }
+        if (direction === 'Prev') {
+            if (monthIdx >= 1) {
+                monthIdx--
+            }
+        }
+        setMonth(datesList[monthIdx].monthName)
     }
     console.log(month)
-    console.log(date)
+    if (date !== '') {
+        console.log(date)
+    }
     
     return (
         <div id='calendar-view'>
-            <span onClick={() => setMonth(datesList[0].monthName)}>Prev</span> 
-            <span>{month}</span> <span onClick={() => setMonth(datesList[1].monthName)}>Next</span>
+            <span onClick={() => handleMonthChange('Prev')}>Prev</span> 
+            <span>{month}</span> <span onClick={() => handleMonthChange('Next')}>Next</span>
             <ul>{month !== '' ? datesList.find(item => item.monthName === month).datesFromMonth.map(givenDate => <li onClick={() => setDate(givenDate)}>{givenDate}</li>): ''}</ul>
         </div>
     )
