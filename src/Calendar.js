e547e7ec4d0df961a134b1fb996446e221dab20f
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-05-14 17:35:36 +1000

    Available dates are now shown in Modal component

--- src/Calendar.js
+++ src/Calendar.js
@@ -5,47 +5,71 @@ const Calendar = (dates) => {
     dates = dates.dates
     const [month, setMonth] = useState('')
     const [date, setDate] = useState('')
-    let monthIdx = 0
+    const [open, setOpen] = useState(false)
+    // on choosing date, make request and redirect
+    if (dates.length !== 0) {
 
-    const getMonth = (date) => {
-        const givenMonth = date.slice(date.indexOf('-')+1, date.lastIndexOf('-'))
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-        return months[Number(givenMonth)-1]
-    }
-    const datesList = []
-    dates.map(month => {
-        const monthObj = {monthName: getMonth(month[0]), datesFromMonth: month}
-        datesList.push(monthObj)
-    })
-    if (month === '') {
-        setMonth(datesList[monthIdx].monthName)
-    }
-    const handleMonthChange = (direction) => {
-        if (direction === 'Next') {
-            if (monthIdx <= datesList.length-1) {
-                monthIdx++
-            }
-        }
-        if (direction === 'Prev') {
-            if (monthIdx >= 1) {
-                monthIdx--
-            }
-        }
-        setMonth(datesList[monthIdx].monthName)
-    }
-    console.log(month)
-    if (date !== '') {
-        console.log(date)
-    }
+        let monthIdx = 0
     
-    return (
-        <div id='calendar-view'>
-            <span onClick={() => handleMonthChange('Prev')}>Prev</span> 
-            <span>{month}</span> <span onClick={() => handleMonthChange('Next')}>Next</span>
-            <ul>{month !== '' ? datesList.find(item => item.monthName === month).datesFromMonth.map(givenDate => <li onClick={() => setDate(givenDate)}>{givenDate}</li>): ''}</ul>
-        </div>
-    )
+        const getMonth = (date) => {
+            const givenMonth = date.slice(date.indexOf('-')+1, date.lastIndexOf('-'))
+            const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    
+            return months[Number(givenMonth)-1]
+        }
+    
+        const datesList = []
+        dates.map(month => {
+            const monthObj = {monthName: getMonth(month[0]), datesFromMonth: month}
+            datesList.push(monthObj)
+        })
+    
+        if (month === '') {
+            setMonth(datesList[monthIdx].monthName)
+        }
+    
+        const handleMonthChange = (direction) => {
+            if (direction === 'Next') {
+                if (monthIdx <= datesList.length-1) {
+                    monthIdx++
+                }
+            }
+            if (direction === 'Prev') {
+                if (monthIdx >= 1) {
+                    monthIdx--
+                }
+            }
+            setMonth(datesList[monthIdx].monthName)
+        }
+        
+        // styles
+        if (date !== '') {
+            console.log(date)
+        }
+    
+        const padding = {
+            padding: 5
+        }
+    
+        return (
+            <div id='calendar-view'>
+                <button onClick={() => setOpen(true)}>Change Date</button>
+                <Modal isOpen={open}>
+                    <button onClick={() => setOpen(false)}>Close</button>
+                    <span onClick={() => handleMonthChange('Prev')} style={padding}>Prev</span> 
+                    <span style={padding}>{month}</span> <span onClick={() => handleMonthChange('Next')} style={padding}>Next</span>
+                    <ul>{month !== '' ? datesList.find(item => item.monthName === month).datesFromMonth.map(givenDate => <li onClick={() => {
+                        setDate(givenDate)
+                        setOpen(false)
+                        }}>{givenDate}</li>): ''}
+                    </ul>
+                </Modal>
+            </div>
+        )
+    }
+    else {
+        return <Modal isOpen={true}>Waiting for available dates to be retrieved from the server ...</Modal>
+    }
 }
 
 export default Calendar;
