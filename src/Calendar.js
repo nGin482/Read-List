ac0fc745bb97a4c38d2bdc82a570c151ebb512e2
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-05-15 16:26:03 +1000

    Starting to add styles to the calendar modal

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Modal from 'react-modal';
+import './Calendar.css';
 
 const Calendar = (dates) => {
     dates = dates.dates
@@ -42,27 +43,31 @@ const Calendar = (dates) => {
             setMonth(datesList[monthIdx].monthName)
         }
         
-        // styles
         if (date !== '') {
             console.log(date)
-        }
-    
-        const padding = {
-            padding: 5
         }
         
         const selectDate = (givenDate) => {
             setDate(givenDate)
             setOpen(false)
         }
+        Modal.setAppElement('#root')
+        const customStyles = {
+            content: {
+                backgroundColor: 'rgb(251, 130, 97)'
+            }
+        }
     
         return (
-            <div id='calendar-view'>
-                <button onClick={() => setOpen(true)}>Change Date</button>
-                <Modal isOpen={open}>
-                    <button onClick={() => setOpen(false)}>Close</button>
-                    <span onClick={() => handleMonthChange('Prev')} style={padding}>Prev</span> 
-                    <span style={padding}>{month}</span> <span onClick={() => handleMonthChange('Next')} style={padding}>Next</span>
+            <div id="modal">
+            <button onClick={() => setOpen(true)}>Change Date</button>
+                <Modal isOpen={open} id="calendar-view" style={customStyles}>
+                    <button onClick={() => setOpen(false)}>Close</button> <br/>
+                    <div id="headers">
+                        <div id="month-change-prev"><span onClick={() => handleMonthChange('Prev')}>Prev</span></div>
+                        <div id="month-shown"><span>{month}</span></div>
+                        <div id="month-change-next"><span onClick={() => handleMonthChange('Next')}>Next</span></div>
+                    </div>
                     <ul>{month !== '' ? datesList.find(item => item.monthName === month).datesFromMonth.map(givenDate => <li onClick={() => selectDate(givenDate)}>{givenDate}</li>): ''}
                     </ul>
                 </Modal>
@@ -70,6 +75,7 @@ const Calendar = (dates) => {
         )
     }
     else {
+        Modal.setAppElement('#root')
         return <Modal isOpen={true}>Waiting for available dates to be retrieved from the server ...</Modal>
     }
 }
