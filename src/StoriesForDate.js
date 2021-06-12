894def45595c907912a892efd10da35959b17f94
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-06-12 17:52:37 +1000

    The stories from the chosen date now render

--- src/StoriesForDate.js
+++ src/StoriesForDate.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router';
 import services from './services/services.js';
+import StoryList from './StoryList.js';
 
 const StoriesForDate = () => {
     const date = useParams().date
     console.log(date)
+    const [stories, setStories] = useState([])
 
-    services.getDateStories(date).then(data => {
-        console.log(data);
-    })
+    useEffect(() => {
+        services.getDateStories(date).then(data => {
+            setStories(data)
+        })
+    }, []
+    )
 
     return (
-        ''
+        <StoryList stories={stories}/>
     )
 }
 
