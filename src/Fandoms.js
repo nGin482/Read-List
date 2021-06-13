76a09a5ecf31817440b19c907362a25ecf093a93
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-06-13 16:15:32 +1000

    Added modal and new structure

--- src/Fandoms.js
+++ src/Fandoms.js
@@ -1,17 +1,27 @@
 import React, {useState, useEffect} from 'react';
+import Modal from 'react-modal';
 
 const Fandoms = () => {
+    const [open, setOpen] = useState(false)
+
     return (
-        <ul>
-            <li>Code Geass</li>
-            <li>Doctor Who</li>
-            <li>Endeavour</li>
-            <li>Lewis</li>
-            <li>NCIS</li>
-            <li>NCIS: LA</li>
-            <li>Person of Interest</li>
-            <li>Transformers</li>
-        </ul>
+        <div id="fandoms-page">
+            <ul id="fandoms-list">
+                <li>Code Geass</li>
+                <li>Doctor Who</li>
+                <li>Endeavour</li>
+                <li>Lewis</li>
+                <li>NCIS</li>
+                <li>NCIS: LA</li>
+                <li>Person of Interest</li>
+                <li>Transformers</li>
+            </ul>
+            <span onClick={() => setOpen(true)}>Add a new fandom</span>
+            <Modal isOpen={open} id="add-fandom">
+
+                <button id="close-fandom-modal" onClick={() => setOpen(false)}>Close</button>
+            </Modal>
+        </div>
     )
 }
 
