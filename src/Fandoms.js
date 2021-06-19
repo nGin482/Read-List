72bdd6523c6c96c11e4901a73e3defc6be163ec7
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-06-19 12:55:10 +1000

    Update Fandom modal used in Fandoms component

--- src/Fandoms.js
+++ src/Fandoms.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Modal from 'react-modal';
 import services from './services/services.js';
 import AddFandom from './AddFandom.js';
+import UpdateFandom from './UpdateFandom.js';
 import './Fandoms.css';
 
 import ffn_logo from './images/FF.Net_Logo.png'
@@ -10,6 +11,7 @@ import ao3_logo from './images/Archive_of_Our_Own_logo.png'
 const Fandoms = () => {
     const [fandoms, setFandoms] = useState([])
     const [openAdd, setOpenAdd] = useState(false)
+    const [openUpdate, setOpenUpdate] = useState(false)
     
     const [message, setMessage] = useState('')
 
@@ -25,6 +27,11 @@ const Fandoms = () => {
         setMessage('')
     }
 
+    const openUpdateModal = () => {
+        setOpenUpdate(true)
+        setMessage('')
+    }
+
     if (fandoms.length !== 0) {
         return (
             <div id="fandoms-page">
@@ -36,11 +43,12 @@ const Fandoms = () => {
                         {fandom.FFN !== '' ? <img src={ffn_logo} className="ffn-logo" alt="Stories from Fanfiction.Net are being recorded"/> : ''}
                         {fandom.AO3 !== '' ? <img src={ao3_logo} className="ao3-logo" alt="Stories from Archive of our Own are being recorded"/> : ''}
                         <br/>
-                        <span id="open-update-modal" onClick={() => console.log('update fandom')}>Update this fandom</span>
+                        <span id="open-update-modal" onClick={() => openUpdateModal()}>Update this fandom</span>
                         </div>
                     )}
                 </ul>
                 <AddFandom openAdd={openAdd} setOpenAdd={setOpenAdd} message={message} setMessage={setMessage}/>
+                <UpdateFandom openUpdate={openUpdate} setOpenUpdate={setOpenUpdate} message={message} setMessage={setMessage}/>
             </div>
         )
     }
