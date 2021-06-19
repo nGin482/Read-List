6305ca46209b1123951032f26a99fff9f61d685c
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2021-06-19 23:23:33 +1000

    Changing styles for delete fandom modal

--- src/DeleteFandom.js
+++ src/DeleteFandom.js
@@ -19,10 +19,10 @@ const DeleteFandom = ({fandomName, openDelete, setOpenDelete, message, setMessag
         <Modal isOpen={openDelete} id="delete-fandom-modal">
             <button id="close-fandom-modal" onClick={() => setOpenDelete(false)}>Close</button>
             <h3 className="header" id="delete-header">Delete {fandomName}</h3>
-            <p id="delete-fandom-message">Are you sure you want to delete {fandomName} from the archives list?</p>
+            <p className="message" id="delete-fandom-question">Are you sure you want to delete {fandomName} from the archives list?</p>
             <button className="confirm-delete-options" id="confirm-delete-yes" onClick={() => setDeleteConfirm(true)}>Yes</button><br/>
             <button className="confirm-delete-options" id="confirm-delete-no" onClick={() => setOpenDelete(false)}>No</button>
-            <p id="delete-fandom-message">{message}</p>
+            <p className="message" id="delete-fandom-message">{message}</p>
         </Modal>
     )
 }
