d1f1d61841ed567127d12de2c88b78c50c704228
nGin482 <ngin2547@gmail.com>
2021-07-02 22:15:24 +1000

    Using the validation functions from src/utils

--- src/AddFandom.js
+++ src/AddFandom.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Modal from 'react-modal';
 import services from './services/services.js';
+import utilFunctions from './utils.js';
 import './AddFandom.css';
 import './modal.css'
 
@@ -11,102 +12,41 @@ const AddFandom = ({openAdd, setOpenAdd, message, setMessage}) => {
     const [ao3_url, setAO3_url] = useState('')
     const [search, setSearch] = useState('')
 
+    if (!openAdd) {
+        setFandom('')
+        setFFN_url('')
+        setAO3_url('')
+        setSearch('')
+    }
+    
     const addFandom = (event) => {
         event.preventDefault()
-        let approved = true
 
-        const checkValidURL = (url) => {
-            if (!url.includes('https://')) {
-                setMessage('This is not a valid URL. Please make sure the given details is valid before submitting')
-                approved = false
-            }
-        }
-        const checkValidFFNURL = () => {
-            if (!ffn_url.includes('fanfiction.net')) {
-                setMessage('This is not a valid Fanfiction.Net URL. Please make sure the URL given is from Fanfiction.Net before submitting')
-                approved = false
-            }
-        }
-        const checkValidAO3URL = () => {
-            if (!ao3_url.includes('archiveofourown.org')) {
-                setMessage('This is not a valid AO3 URL. Please make sure the URL given is from Archive of our Own before submitting')
-                approved = false
-            }
-        }
-        const checkBothValidURLs = () => {
-            if (!ffn_url.includes('https://') && ao3_url.includes('https://')) {
-                setMessage('The URL given for Fanfiction.Net is not valid. Please make sure the given details is valid before submitting')
-                approved = false
-            }
-            else if (ffn_url.includes('https://') && !ao3_url.includes('https://')) {
-                setMessage('The URL given for Archive of our Own is not valid. Please make sure the given details is valid before submitting')
-                approved = false
-            }
-            else if (!ffn_url.includes('https://') && !ao3_url.includes('https://')) {
-                setMessage('The URLs given for both sites are not valid. Please make sure the given details is valid before submitting')
-                approved = false
-            }
-            else {
-                if (!ffn_url.includes('fanfiction.net') && ao3_url.includes('archiveofourown.org')) {
-                    setMessage('This is not a valid Fanfiction.Net URL. Please make sure the URL given is from Fanfiction.Net before submitting')
-                    approved = false
-                }
-                else if (ffn_url.includes('fanfiction.net') && !ao3_url.includes('archiveofourown.org')) {
-                    setMessage('This is not a valid AO3 URL. Please make sure the URL given is from Archive of our Own before submitting')
-                    approved = false
-                }
-                else if (!ffn_url.includes('fanfiction.net') && !ao3_url.includes('archiveofourown.org')) {
-                    setMessage('The Fanfiction.Net and the Archive of our Own URLs given do not come from their respective sites. Please make sure they are correct URLs before submitting')
-                    approved = false
-                }
-            }
-        }
+        const checkAddFandom = utilFunctions.checkValidationAddFandom(fandom, ffn_url, ao3_url, search)
         
-        if (fandom !== '' && (ffn_url !== '' || ao3_url !== '') && search !== '') {
-            if (ffn_url !== '') {
-                checkValidURL(ffn_url)
-                checkValidFFNURL()
+        if (checkAddFandom.status) {
+            const fandom_object = {
+                fandom: fandom,
+                ffn_url: ffn_url,
+                ao3_url: ao3_url,
+                search: search
             }
-            if (ao3_url !== '') {
-                checkValidURL(ao3_url)
-                checkValidAO3URL()
-            }
-            if (ffn_url !== '' && ao3_url !== '') {
-                checkBothValidURLs()
-            }
-            if (approved) {
-                const fandom_object = {
-                    fandom: fandom,
-                    ffn_url: ffn_url,
-                    ao3_url: ao3_url,
-                    search: search
-                }
-                services.addFandom(fandom_object).then(data => {
-                    setFandom('')
-                    setFFN_url('')
-                    setAO3_url('')
-                    setMessage(data.message)
-                }).catch(err => {
-                    setFandom('')
-                    setFFN_url('')
-                    setAO3_url('')
-                    setMessage(err.response.data.message)
-                })
-            }
+            services.addFandom(fandom_object).then(data => {
+                setFandom('')
+                setFFN_url('')
+                setAO3_url('')
+                setSearch('')
+                setMessage(data.message)
+            }).catch(err => {
+                setFandom('')
+                setFFN_url('')
+                setAO3_url('')
+                setSearch('')
+                setMessage(err.response.data.message)
+            })
         }
         else {
-            if (fandom === '' && ffn_url === '' && ao3_url === '') {
-                setMessage('The fandom and URL fields must not be blank. Please specify the name of the fandom and provide one or both site URLs.')
-            }
-            else if (fandom === '' && (ffn_url !== '' || ao3_url !== '')) {
-                setMessage('Please specify the name of the fandom before submitting.')
-            }
-            else if (ffn_url === '' && ao3_url === '') {
-                setMessage('Please provide one or both site URLs.')
-            }
-            else if (search === '') {
-                setMessage('Please specify how many pages you would like to search.')
-            }
+            setMessage(checkAddFandom.message)
         }
     }
 
