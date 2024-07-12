a30aeeba2564c2850cce8aff5f7810d3a7997e74
nGin482 <ngin2547@gmail.com>
2024-07-13 09:51:02 +1000

    SA-3 Database class

---
+++ server/database/index.ts
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize"
+
+
+class Database extends Sequelize {
+    
+    constructor() {
+        super(process.env.DB_URL, {
+            dialect: 'postgres',
+            logging: false
+        });
+        this.testConnection();
+    }
+
+    async testConnection() {
+        try {
+            await this.authenticate()
+            console.log('Database connection successfully established');
+        }
+        catch(error) {
+            console.error('Unable to connect to database');
+            console.error(error)
+        }
+    };
+};
+
+const database = new Database();
+
+export { database };
