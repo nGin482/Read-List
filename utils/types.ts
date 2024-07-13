6ab7ba29fb5ffe8e7b2fd7d4fc799d641375e42e
nGin482 <ngin2547@gmail.com>
2024-07-13 12:22:04 +1000

    SA-3 Types for models

---
+++ utils/types.ts
@@ -0,0 +1,63 @@
+export interface FandomArchive {
+    id: number
+    name: string
+    ffn_url: string
+    ao3_url: string
+    search: 'One' | 'Many'
+    ignore_stories: string[]
+}
+
+interface IBaseStory {
+    storyID: number,
+    title: string
+    author: string
+    summary: string
+    fandoms: string[]
+    characters: string[]
+    relationships: string[]
+    chapters: number
+    words: number
+    publishedDate: string
+    updatedDate: string
+    collectedDate: Date
+    readStatus: boolean
+    dateRead: Date
+    rating: string
+    status: 'In Progress' | 'Complete'
+    url: string
+}
+
+
+export interface IFFNStory extends IBaseStory {
+    genres: string[] 
+    date: string
+    archive: 'Fanfiction.Net'
+}
+
+export interface IAO3Story extends IBaseStory {
+    categories: string[]
+    tags: string[]
+    warnings: string[]
+    archive: 'Archive of our Own'
+}
+
+export type IStory = IFFNStory | IAO3Story
+
+export interface IArchiveStories {
+    fandom: string
+    AO3_URL: IStory[]
+    FFN_URL: IStory[]
+}
+
+export interface Collection {
+    date: string
+    stories: IArchiveStories[]
+}
+
+export interface Characters {
+    id: number
+    name: string
+    fandom_id: number
+}
+
+export type SearchOptions = 'title' | 'fandoms';
