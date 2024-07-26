cedf2d5367572f2ceab402d5386f51d42b3419a8
nGin482 <ngin2547@gmail.com>
2024-07-26 19:15:17 +1000

    SA-4 Stories API client

---
+++ src/services/StoriesAPI.ts
@@ -0,0 +1,47 @@
+
+
+import { APIClient } from "./APIClient";
+import { IStory, ReadingStatus } from '../../utils/types';
+
+export class StoriesAPI extends APIClient {
+
+    private static apiClient: APIClient = new APIClient();
+    private static baseEndpoint = '/stories';
+
+    static async getStoryById(id: string) {
+        const story = await this.apiClient.get<IStory>(`${this.baseEndpoint}/${id}`);
+
+        return story;
+    };
+
+    static async getReadingList() {
+        const readingList = await this.apiClient.get<IStory[]>(this.baseEndpoint + '/reading-list');
+        return readingList;
+    };
+
+    static async getCompletedList() {
+        const completedList = await this.apiClient.get<IStory[]>(this.baseEndpoint + '/complete-list');
+        return completedList;
+    };
+
+    static async updateStoryDetails(story: IStory) {
+        const updatedStory = await this.apiClient.put<IStory, IStory>(
+            `${this.baseEndpoint}/${story.storyId}`,
+            story
+        );
+
+        return updatedStory;
+    };
+
+    static async updateReadingStatus(storyId: number, status: ReadingStatus) {
+        const updatedStory = await this.apiClient.patch<{ status: ReadingStatus }, IStory>(
+            this.baseEndpoint + storyId,
+            { status }
+        );
+        return updatedStory;
+    };
+
+    static async deleteStory(storyId: number) {
+        await this.apiClient.delete(this.baseEndpoint + `/${storyId}`);
+    };
+}
