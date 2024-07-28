1d28f2dcad73491bad2126cb94de465983afc09a
nGin482 <ngin2547@gmail.com>
2024-07-28 14:52:47 +1000

    SA-6 Add warning alert to StoryList component if it has no stories

--- src/Pages/StoryPage.tsx
+++ src/Pages/StoryPage.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { notification, Spin } from 'antd';
 
 import Story from '../components/Story/index.js';
-import services from '../services/services.js';
+// import services from '../services/services.js';
 import { IStory } from '../utils/types.js';
 
 const StoryPage = () => {
@@ -11,13 +11,13 @@ const StoryPage = () => {
     const { id } = useParams<{ id: string }>();
     
     useEffect(() => {
-        services.getStoryById(id)
-            .then(setStory)
-            .catch(err => {
-                notification.error({
-                    message: err?.response.status === 404 ? 'The story could not be found' : 'An error occurred'
-                });
-        });
+        // services.getStoryById(id)
+        //     .then(setStory)
+        //     .catch(err => {
+        //         notification.error({
+        //             message: err?.response.status === 404 ? 'The story could not be found' : 'An error occurred'
+        //         });
+        // });
     }, [id]);
 
     
@@ -25,7 +25,7 @@ const StoryPage = () => {
         <>
             {story ? (
                 <div id='page-contents'>
-                    <Story story={story} view="browsing" />
+                    <Story story={story} />
                 </div>
             ) : (
                 <Spin fullscreen tip="Waiting for the story to load" />
